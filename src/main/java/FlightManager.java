e17db5fef4ae063b7fb273228ad490d53f25fb90
juanMoleroD <juanldimeco@gmail.com>
2022-09-23 14:50:46 +0100

    added flightmanager class.

---
+++ src/main/java/FlightManager.java
@@ -0,0 +1,11 @@
+public class FlightManager {
+    private Flight flight;
+
+    public FlightManager(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+}
